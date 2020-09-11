92019985bdffb034d8203ab1fa29ec54ce16cfc5
Diogo <dioggo.soares@live.com>
2020-09-11 01:20:10 -0300

    feat: robot text

---
+++ robots/text.js
@@ -0,0 +1,12 @@
+function robot(content){
+  fetchContentFromWikipedia(content)
+  //sanitizeContent(content)
+  //breakContentIntoSentences(content)
+  //console.log(`Recebi com sucesso o content: ${content.searchTerm}`)
+
+  function fetchContentFromWikipedia(content){
+
+  }
+}
+
+module.exports = robot
